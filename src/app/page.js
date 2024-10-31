cd070922284bcc2cfd12d0f83ad93fb0f7ad46c7
fe705 <maria.f.almeida@iesb.edu.br>
2024-10-31 20:24:45 -0300

    atividade9

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,71 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
 
-export default function Home() {
+import Pagina from "@/components/Pagina";
+import { Button, Card, Col, Row } from "react-bootstrap";
+
+export default function HomePage() {
+  const faculdades = JSON.parse(localStorage.getItem("faculdades")) || [];
+  const cursos = JSON.parse(localStorage.getItem("cursos")) || [];
+  const professores = JSON.parse(localStorage.getItem("professores")) || [];
+  const disciplinas = JSON.parse(localStorage.getItem("disciplinas")) || [];
+  const alunos = JSON.parse(localStorage.getItem("alunos")) || [];
+
+  const lista = [
+    {
+      nome: "Faculdades",
+      imagem:
+        "https://i.pinimg.com/236x/53/f4/63/53f463fcc23af8ec4fc28e7c5ccd168a.jpg",
+      quantidade: faculdades.length,
+      link: "/faculdades",
+    },
+    {
+      nome: "Cursos",
+      imagem:
+        "https://i.pinimg.com/474x/b2/1d/08/b21d0843c3a0d5f5586222644bf402cd.jpg",
+      quantidade: cursos.length,
+      link: "/cursos",
+    },
+    {
+      nome: "Professores",
+      imagem:
+        "https://i.pinimg.com/736x/39/09/fb/3909fb65bbab271bb5a9ddbf85c80d00.jpg",
+      quantidade: professores.length,
+      link: "/professores",
+    },
+    {
+      nome: "Disciplinas",
+      imagem:
+        "https://i.pinimg.com/236x/79/0f/fc/790ffceeb183a1059b77c5558342ffc0.jpg",
+      quantidade: disciplinas.length,
+      link: "/disciplinas",
+    },
+    {
+      nome: "Alunos",
+      imagem:
+        "https://i.pinimg.com/236x/ce/96/4d/ce964d843b92374b8b96e105ffa82831.jpg",
+      quantidade: alunos.length,
+      link: "/alunos",
+    },
+  ];
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <Pagina titulo={"Projeto IESB"}>
+      <Row md={4}>
+        {lista.map((item) => (
+          <Col className="py-2">
+            <Card style={{ height: "100%" }}>
+              <Card.Img src={item.imagem} style={{ height: "100%" }} />
+              <Card.Body>
+                <Card.Title>{item.nome}</Card.Title>
+                Cadastrados: {item.quantidade}
+              </Card.Body>
+              <Card.Footer className="text-end">
+                <Button href={item.link}>Ver Lista</Button>
+              </Card.Footer>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Pagina>
   );
 }
